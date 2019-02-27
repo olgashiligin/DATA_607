cd16f0ba781b277d7f67b891b9ed08cc14bf2d71
olgashiligin <39236319+olgashiligin@users.noreply.github.com>
2019-02-27 17:31:39 +0000

    Add files via upload

---
+++ assignment2/movies.sql
@@ -0,0 +1,44 @@
+
+create table movies (
+   id int(4),
+   name varchar(50),
+   movie varchar(100),
+   rating float(3,1)
+
+);
+
+INSERT INTO movies
+    (ID, name, movie,rating )
+VALUES
+    (1,"Olivia","Forrest Gump",5),
+    (1,"Olivia","The Godfather",4),
+    (1,"Olivia","The Dark Knight",4),
+    (1,"Olivia","The Shawshank Redemption",3),
+    (1,"Olivia","Star Wars",5),
+    (1,"Olivia","The Matrix",3),
+    (2,"Vlad","Forrest Gump",4),
+    (2,"Vlad","The Godfather",5),
+    (2,"Vlad","The Dark Knight",4),
+    (2,"Vlad","The Shawshank Redemption",5),
+    (2,"Vlad","Star Wars",5),
+    (2,"Vlad","The Matrix",5),
+    (3,"Kate","Forrest Gump",5),
+    (3,"Kate","The Godfather",3),
+    (3,"Kate","The Dark Knight",5),
+    (3,"Kate","The Shawshank Redemption",3),
+    (3,"Kate","Star Wars",3),
+    (3,"Kate","The Matrix",5),
+    (4,"Mick","Forrest Gump",3),
+    (4,"Mick","The Godfather",5),
+    (4,"Mick","The Dark Knight",5),
+    (4,"Mick","The Shawshank Redemption",4),
+    (4,"Mick","Star Wars",5),
+    (4,"Mick","The Matrix",5),
+    (5,"Steve","Forrest Gump",5),
+    (5,"Steve","The Godfather",5),
+    (5,"Steve","The Dark Knight",5),
+    (5,"Steve","The Shawshank Redemption",5),
+    (5,"Steve","Star Wars",5),
+    (5,"Steve","The Matrix",5);
+
+
